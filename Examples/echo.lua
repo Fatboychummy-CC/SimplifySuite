47129252a7acbf7edccbc8a115e9cbc0baa811e4
Matthew W <fatboychummy@gmail.com>
2021-11-18 21:26:19 -0700

    Add the example files.

---
+++ Examples/echo.lua
@@ -0,0 +1,6 @@
+--[=====[5]=====]
+-- Change the number above locally, then update (with cache force refresh if needed) to cause an update.
+
+-- This is an example "repository" - pretend this file is a single repo on github.
+local printer = require "printer"
+printer.print(...)
