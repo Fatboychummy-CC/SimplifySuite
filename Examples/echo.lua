b8d8df763ff78c5484f84739a3b6063bb0515fbd
Matthew W <fatboychummy@gmail.com>
2022-02-21 15:06:47 -0700

    We're doing it again!

--- Examples/echo.lua
+++
@@ -1,6 +0,0 @@
---[=====[5|Example-Repo]=====]
--- Change the number above locally, then update (with cache force refresh if needed) to cause an update.
-
--- This is an example "repository" - pretend this file is a single repo on github.
-local printer = require "printer"
-printer.print(...)
