98b00c64a668e8610eebb216b3b890fcb22c5d5b
Matthew W <fatboychummy@gmail.com>
2022-02-22 19:09:24 -0700

    Update the help function to packages instead of modules

--- install.lua
+++ install.lua
@@ -6,6 +6,11 @@
 local argparser = require "common.argparser"
 local fileutil = require "common.fileutil"
 local tprint = require "common.tabbedprint"
+local logger = require "common.log"
+
+local PACKAGE_ROOT = "https://raw.githubusercontent.com/Fatboychummy-CC/SimplifySuite/main/packages/"
+local ALL_PACKAGES_URL = PACKAGE_ROOT .. "all_packages.csv?token=GHSAT0AAAAAABRQ3DWE4CICAIPJCZGPH5LUYQUH5WQ"
+local log = logger.create("INST")
 
 local args = argparser.parse(...)
 
@@ -14,15 +19,15 @@ local function printHelp()
   term.setTextColor(colors.yellow)
   write("  > ")
   term.setTextColor(colors.lightGray)
-  tprint.print(prog, "[flags] <'get-modules' / module[s]>")
+  tprint.print(prog, "[flags] <'get-packages' / package[s]>")
   print()
   term.setTextColor(colors.orange)
   print("Flags:")
   term.setTextColor(colors.blue)
   tprint.printAt(3, "-f/--force:")
   term.setTextColor(colors.white)
-  tprint.printAt(5, "Force installation of the module, even if it already exists.")
-  tprint.printAt(5, "This will delete the old module.")
+  tprint.printAt(5, "Force installation of the package, even if it already exists.")
+  tprint.printAt(5, "This will delete the old package.")
   term.setTextColor(colors.blue)
   tprint.printAt(3, "-v/--verbose:")
   term.setTextColor(colors.white)
@@ -31,13 +36,13 @@ local function printHelp()
   term.setTextColor(colors.orange)
   print("Arguments:")
   term.setTextColor(colors.blue)
-  tprint.printAt(3, "get-modules:")
+  tprint.printAt(3, "get-packages:")
   term.setTextColor(colors.white)
-  tprint.printAt(5, "Grabs all available modules and lists them, sorted alphabetically by name.")
+  tprint.printAt(5, "Grabs all available packages and lists them, sorted alphabetically by name.")
   term.setTextColor(colors.blue)
-  tprint.printAt(3, "<module>:")
+  tprint.printAt(3, "<package[s]>:")
   term.setTextColor(colors.white)
-  tprint.printAt(5, "Install a module with name given by the argument.")
+  tprint.printAt(5, "Install a package (or packages) with name given by the argument.")
 end
 
 if args.flags.h or args.flags.help or args.args.n == 0 then
